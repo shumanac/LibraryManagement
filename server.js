aed657ed50a4b627b0c6b1e967bc7bd196906579
Shumana Chowdhury <shumanachowdhury@MacBook-Air.home>
2017-02-18 00:25:59 -0500

    mongodb connected

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require ('morgan'); /* Morgan is used for logging request details*/
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/library', function(err){
+    if(err){
+        console.log('Not connected to the database'+ err);
+    }else{
+        console.log('Successfully connected to mongodb');
+    }
+})
 
 app.use(morgan('dev'));
 app.listen(port, function(){
