395e5ee3a380b72ca57f71ede252d7cdcd213b70
Shumana Chowdhury <shumanachowdhury@MacBook-Air.home>
2017-02-17 23:11:12 -0500

    morgan added

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8080;
+var morgan = require ('morgan'); /* Morgan is used for logging request details*/
 
-app.listen(8080, function(){
-    console.log("Running the server");
+app.use(morgan('dev'));
+app.listen(port, function(){
+    console.log("Running the server on port"+' ' + port);
 });
