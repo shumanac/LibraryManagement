e1fac53c2d507c08606787d0352647599c1c62cd
Shumana Chowdhury <shumanachowdhury@MacBook-Air.home>
2017-02-18 00:59:01 -0500

    user schema checked in database

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require ('morgan'); /* Morgan is used for logging request details*/
 var mongoose = require('mongoose');
+var User = require('./app/models/user');
+var bodyParser = require('body-parser');
+
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 mongoose.connect('mongodb://localhost:27017/library', function(err){
     if(err){
@@ -10,7 +17,16 @@ mongoose.connect('mongodb://localhost:27017/library', function(err){
     }else{
         console.log('Successfully connected to mongodb');
     }
-})
+});
+
+app.post('/users', function(req, res){
+  var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    user.email = req.body.email;
+    user.save();
+    res.send("user created");
+});
 
 app.use(morgan('dev'));
 app.listen(port, function(){
