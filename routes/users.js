ddeeb13f69beaf2b57e1cae4fcf313d4fb4ad7a4
Shumana Chowdhury <shumanachowdhury@MacBook-Air.home>
2017-02-19 03:09:16 -0500

    registration and login working properly

--- routes/users.js
+++ routes/users.js
@@ -64,16 +64,16 @@ passport.use(new LocalStrategy(
   function(username, password, done) {
       User.getUserByUsername(username, function(err, user){
           
-          if(err)throw err;
+          if(err)throw err
           if(!user){
               return done(null, false, {message: 'Unknown User'});
           }
           User.comparePassword(password, user.password, function(err, isMatch){
-              if(err)throw err;
+              if(err)throw err
               if(isMatch){
-                  return done(null, user);
+                  return done(null, user)
               }else{
-                  return done(null, false, {message: 'Invalid Password'});
+                  return done(null, false, {message: 'Invalid Password'})
               }
           });
       });
@@ -85,7 +85,7 @@ passport.serializeUser(function(user, done) {
 
 passport.deserializeUser(function(id, done) {
   User.getUserById(id, function(err, user) {
-    done(err, user);
+    done(err, user)
   });
 });
 
