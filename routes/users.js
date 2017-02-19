0bd2ccfd7a8d42c609ab921aa8ec713fb79a2434
Shumana Chowdhury <shumanachowdhury@MacBook-Air.home>
2017-02-19 01:27:55 -0500

    form validation

--- routes/users.js
+++ routes/users.js
@@ -14,3 +14,31 @@ router.get('/login', function(req, res){
     res.render('login');
 });
 module.exports = router;
+
+//Register user
+
+router.post('/register', function(req, res){
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+    
+    //validation
+    
+    req.checkBody('name', 'Name is required').notEmpty();
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+    
+    var errors = req.validationErrors();
+    if(errors){
+       res.render('register', {
+           errors:errors
+       })
+        
+    }else{
+        console.log("passed");
+    }
+});
